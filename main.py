d7d56022e0826e84d21a4ed63b7ab581729f67b1
kolmlenka <118755490+kolmlenka@users.noreply.github.com>
2023-04-26 21:49:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-n = 30#72218
+n = 30 #72218
 aspect_ratios = set()
 click = -1
 for i in range (0, n+1):
     name = 'unique_uis/combined/' + str(i) + '.json'
-    print(name)
+    #print(name)
     with open(name, "r") as my_file:
         cur_json = my_file.read()
     current = json.loads(cur_json)
@@ -12,7 +12,7 @@ for i in range (0, n+1):
     b = current["activity"]["root"]["bounds"][3]
     res = a/b
     aspect_ratios.add(round(res, 4))
-    print(current["activity"]["root"]["clickable"])
+    #print(current["activity"]["root"]["clickable"])
     if (current["activity"]["root"]["clickable"] == False):
         click += 1
 print('отношения сторон: ', end = '')
