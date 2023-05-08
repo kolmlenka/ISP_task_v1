ad99d188ed17bc6f577f9ad409627e9cf240d5b3
kolmlenka <118755490+kolmlenka@users.noreply.github.com>
2023-05-09 01:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
 from PIL import Image
 import os.path
+import matplotlib.pyplot as plt
 
-n = 2 #72218
+n = 1000 #72218
 aspect_ratios = set()
-click = 0
+count = 0
+y_list = [] #список в котором собираю все глубины деревьев
+x_list = [] #список в котором номера деревьев
 
 def deepth(dictionary, some_list = []):
     for k in dictionary.keys():
@@ -14,33 +17,46 @@ def deepth(dictionary, some_list = []):
             deepth(dictionary[k], some_list)
     return len(some_list)
 
+def clickable(dictionary, click = set()):
+    for k in dictionary.keys():
+        if k == 'clickable':
+            click.add(dictionary[k])
+        elif type(dictionary[k]) is (dict or list):
+            clickable(dictionary[k], click)
+    if False in click:
+        return 1
+    else:
+        return 0
+
 for i in range (0, n+1):
     name_json = 'unique_uis/combined/' + str(i) + '.json'
     name_jpg = 'unique_uis/combined/' + str(i) + '.jpg'
+    
     if (os.path.exists(name_json)):
         with open(name_json, "r") as my_file:
             cur_json = my_file.read()
         current = json.loads(cur_json)
-        print(deepth(current, some_list = []))
-        
-        #flag = True
-        #clickable = set()
-        #while flag == True:
-            #print(current["activity"]["root"]["clickable"])
-            #clickable.add(current["activity"]["root"]["clickable"])
-            #for i in range(len(current["activity"]["root"]["children"])):
-                #print(current["activity"]["root"]["children"][i]["clickable"])
-                #clickable.add(current["activity"]["root"]["children"][i]["clickable"])
-                #for i in range(current["activity"]["root"]["children"][i]["children"]):
-                    #print(current["activity"]["root"]["children"][i]["children"][])
-                    #clickable.add(current["activity"]["root"]["children"][i]["clickable"])
-            #flag = False
+
+        x_list += [i]
+        y_list += [deepth(current, some_list = [])]
+        #print(deepth(current, some_list = []))
+
+        count += clickable(current, click = set())
 
     if (os.path.exists(name_jpg)):
         img = Image.open(name_jpg)
         width, height = img.size
         res = width / height
         aspect_ratios.add(round(res, 4))
+        
 print('отношения сторон: ', end = '')
 print(aspect_ratios)
-print ('неинтерактивных скринов: ' + str(click))
+
+plt.title('График распределения глубин UI деревьев', fontsize = 14, color = 'black')
+plt.xlabel('Номер дерава')
+plt.ylabel('Глубина дерева')
+plt.plot(x_list, y_list)
+plt.grid()
+plt.show()
+
+print ('неинтерактивных скринов: ' + str(count))
